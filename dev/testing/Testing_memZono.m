ae790253888ff51d050bf0c337f3b0be79828a0d
Jonas Wagner <jonas.wagner.2826@gmail.com>
2023-10-19 14:46:59 -0500

    fixed minSum to account for misordered factors

--- dev/testing/Testing_memZono.m
+++ dev/testing/Testing_memZono.m
@@ -2,8 +2,7 @@ clear
 Z = zono(eye(2),ones(2,1));
 
 temp = memZono(Z,'test');
-temp2 = memZono(2*Z,{'test_1','test_3'});
+temp2 = memZono(2*Z,{'test_2','test_3'});
 
 % minSum(temp,temp2)
-% 5*temp2
-temp+temp2
+% temp+temp2
--- toolbox/@memZono/minSum.m
+++ toolbox/@memZono/minSum.m
@@ -8,16 +8,18 @@ function obj = minSum(obj1,obj2)
     [k1,ks,k2] = memZono.getUniqueKeys(obj1.factorKeys,obj2.factorKeys);
 
     % Boolean maps for shared factors <--- make clearer?
-    bs1 = ismember(obj1.factorKeys,ks);
-    bs2 = ismember(obj2.factorKeys,ks);
+    [~,idxk1] = ismember(k1,obj1.factorKeys);
+    [~,idxks1] = ismember(ks,obj1.factorKeys);
+    [~,idxks2] = ismember(ks,obj2.factorKeys);
+    [~,idxk2] = ismember(k2,obj2.factorKeys);
 
     % Data selection
-    G_ = [obj1.G(:,~bs1),obj1.G(:,bs1)+obj2.G(:,bs2), obj2.G(:,~bs2)];
+    G_ = [obj1.G(:,idxk1),obj1.G(:,idxks1)+obj2.G(:,idxks2), obj2.G(:,idxk2)];
     c_ = obj1.c + obj2.c;
-    A_ = [obj1.A(:,~bs1), obj1.A(:,bs1), zeros(obj1.nC,obj2.nG);
-            zeros(obj2.nC,obj1.nG), obj2.A(:,bs2), obj2.A(:,~bs2)];
+    A_ = [obj1.A(:,idxk1), obj1.A(:,idxks1), zeros(obj1.nC,obj2.nG);
+            zeros(obj2.nC,obj1.nG), obj2.A(:,idxks2), obj2.A(:,idxk2)];
     b_ = [obj1.b; obj2.b];
-    vset_ = [obj1.vset(~bs1),obj1.vset(bs1),obj2.vset(~bs2)]; %<--- add check for c/d the same?
+    vset_ = [obj1.vset(idxk1),obj1.vset(idxks1),obj2.vset(idxk2)]; %<--- add check for c/d the same?
 
     % Labeling
     keys_.factors = [k1,ks,k2];
