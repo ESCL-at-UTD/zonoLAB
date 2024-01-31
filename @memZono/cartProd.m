7f556df2354410edbff253f97843bb16cdf7598c
Jonas Wagner <jonas.wagner.2826@gmail.com>
2024-01-30 18:51:02 -0600

    fixed issues as requested

--- @memZono/cartProd.m
+++ @memZono/cartProd.m
@@ -1,7 +1,8 @@
-function obj = cartProd(obj1,obj2)
+function obj = cartProd(obj1,obj2,cdsKeyPrefix)
     arguments
         obj1 memZono
         obj2 memZono
+        cdsKeyPrefix = 'cds';
     end
 
     %% Keys
@@ -9,95 +10,91 @@ function obj = cartProd(obj1,obj2)
     [k1,ks,k2] = memZono.getUniqueKeys(obj1.factorKeys,obj2.factorKeys);
     [idxk1,idxks1,idxks2,idxk2] = memZono.getKeyIndices(obj1.factorKeys,obj2.factorKeys);
     % shared dims
-    if isempty(obj1.dimKeys); obj1.dimKeys = 'd1'; end
-    if isempty(obj2.dimKeys); obj2.dimKeys = 'd2'; end
     [d1,ds,d2] = memZono.getUniqueKeys(obj1.dimKeys,obj2.dimKeys);
     [idxd1,idxds1,idxds2,idxd2] = memZono.getKeyIndices(obj1.dimKeys,obj2.dimKeys);
     % shared cons
     [c1,cs,c2] = memZono.getUniqueKeys(obj1.conKeys,obj2.conKeys);
     [idxc1,idxcs1,idxcs2,idxc2] = memZono.getKeyIndices(obj1.conKeys,obj2.conKeys);
-    % R for repeated dims
-    R = zeros(length(ds),obj1.n);
-    for k = 1:length(ds)
-        i = idxds2(k); j = idxds1(k); R(i,j) = 1;
-    end
 
-    %% Data selection
+    %% Factor-based Memory Cartisian Product
     G_ = [
         obj1.G(idxd1,idxk1), obj1.G(idxd1,idxks1), zeros(length(d1),length(k2));
-        obj1.G(idxds1,idxk1), obj1.G(idxds1,idxks1), zeros(length(ds),length(k2));
         zeros(length(d2),length(k1)), obj2.G(idxd2,idxks2), obj2.G(idxd2,idxk2)
         ];
     c_ = [
         obj1.c(idxd1);
-        obj1.c(idxds1);
         obj2.c(idxd2)
         ];
     A_ = [
         obj1.A(idxc1,idxk1), obj1.A(idxc1,idxks1), zeros(length(c1),length(k2));
-        % (need to ensure unique and not have shared constraints that have different factors)
-        zeros(length(cs), length(k1)), obj1.A(idxcs1,idxks1), zeros(length(cs),length(k2));
-        % obj1.A(idxcs1,idxk1), obj1.A(idxcs1,idxks1), zeros(length(cs),length(k2));
-        % zeros(length(cs),length(k1)), obj2.A(idxcs2,idxks2), obj2.A(idxcs2,idxk2);
         zeros(length(c2),length(k1)), obj2.A(idxc2,idxks2), obj2.A(idxc2,idxk2);
-        % R*obj1.G(:,idxk1), R*obj1.G(:,idxks1)-obj2.G(idxds2,idxks2), -obj2.G(idxds2,idxk2) %<-- intersection term
         ];
     b_ = [
         obj1.b(idxc1);
-        obj1.b(idxcs1);
-        obj2.b(idxcs2);
         obj2.b(idxc2);
-        % obj2.c(idxds2) - R*obj1.c;
         ];
-    if ~isempty(idxds2)
-        A_ = [
-            A_;
-            R*obj1.G(:,idxk1), R*obj1.G(:,idxks1)-obj2.G(idxds2,idxks2), -obj2.G(idxds2,idxk2) %<-- intersection term
+
+    % hybrid Zono
+    if obj1.vset(idxks1) ~= obj1.vset(idxks2)
+        error('c/d factors not lining up');
+        % TODO???: add option for c/d not matching => additonal
+        % factors/constraints?
+    end
+    vset_ = [obj1.vset(idxk1),obj1.vset(idxks1),obj2.vset(idxk2)];
+
+    % Labeling
+    keys_.factors = [k1,ks,k2];
+    keys_.dims = [d1,d2];
+    keys_.cons = [c1,c2];
+
+    %% Shared Dimensions
+    if ~isempty(ds)
+        % Intersection terms
+        R = zeros(length(ds),obj1.n);
+        for k = 1:length(ds)
+            i = idxds2(k); j = idxds1(k); R(i,j) = 1; 
+        end
+        % cds key labels
+        cds{length(ds)} = [];
+        for i = 1:length(ds)
+            cds{k} = sprintf('%s_%s_%d',cdsKeyPrefix,ds{k},k);
+        end
+        % Matrices
+        G_ = [G_;
+            obj1.G(idxds1,idxk1), obj1.G(idxds1,idxks1), zeros(length(ds),length(k2));
+        ];
+        c_ = [c_;
+            obj1.c(idxds1);
+        ];
+        A_ = [A_; 
+            R*obj1.G(:,idxk1), R*obj1.G(:,idxks1)-obj2.G(idxds2,idxks2), -obj2.G(idxds2,idxk2) 
         ];
         b_ = [b_;
             obj2.c(idxds2) - R*obj1.c;
         ];
-    end
-    if obj1.vset(idxks1) ~= obj1.vset(idxks2); error('c/d factors not lining up'); end
-    vset_ = [obj1.vset(idxk1),obj1.vset(idxks1),obj2.vset(idxk2)]; %<--- add check for c/d the same?
 
-    %% Labeling
-    keys_.factors = [k1,ks,k2];
-    keys_.dims = [d1,ds,d2]; %<--- add check for same?
-    % Constraints
-    % incoming constraints... ensuring that cons with same name are
-    % maintainted... should eliminate when con keys are the same
-    % if isempty(cs)
-    %     cs1 = cs; cs2 = [];
-    % else
-    %     cs1 = cs; cs2{length(cs)} = [];
-    %     for i = 1:length(cs)
-    %         cs2{i} = sprintfs('%s_%d',cs{i},2);
-    %     end
-    % end
-    % just keep one copy (but ensure they are the same)
-    % shared dim constraints
-    if isempty(ds)
-        cds = [];
-    else
-        cds{length(ds)} = [];
-        for i = 1:length(ds)
-            max_iter=1000;
-            for j = 1:max_iter
-                cds{i} = sprintf('i%i_%s',j,ds{i});
-                if all(~ismember(cds{i},c1)) && all(~ismember(cds{i},cs)) && all(~ismember(cds{i},c2))
-                    cds{i} = sprintf('i%i_%s',j,ds{i});
-                    break
-                end
-                if j == max_iter
-                    fprintf('ERROR: constraint at max number of iterations\n');
-                end
-            end
+        % Labels
+        keys_.dims = [keys_.dims, ds];
+        keys_.cons = [keys_.cons, cds];
+    end
+
+    %% Shared Constraints
+    if ~isempty(cs)
+        if obj1.A(idxcs1,:) ~= obj2.A(idxcs2,:) || obj1.b(idxcs1) ~= obj2.b(idxcs2)
+            error('Shared Constraints are not identical')
+            % TODO???: add option for non-identical constraints
         end
+        A_ = [A_;
+            zeros(length(cs),length(k1)), obj1.A(idxcs1,idxks1), zeros(length(cs),length(k2))
+        ];
+        b_ = [b_;
+            obj1.b(idxcs1)
+        ];
+        % Labeling
+        keys_.cons = [keys_.cons,cs];
     end
-    keys_.cons = [c1,cs1,cs2,c2,cds];
 
-    % Define memZono
+    %% Define memZono
     obj = memZono(G_,c_,A_,b_,vset_,keys_);
     
 end
