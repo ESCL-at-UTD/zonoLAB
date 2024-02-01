9b7a3a75e0d6300c94dc823c3f13cd3f6dcc5df0
Alyssa Vellucci <amv170001@utdallas.edu>
2024-02-01 13:09:27 -0600

    Added checkSetContain to @conZono and updated conZono2AHPoly to output matrices instead of structure

--- globalFunctions/conZono2AHPoly.m
+++ globalFunctions/conZono2AHPoly.m
@@ -7,26 +7,26 @@
 %   Inputs:
 %       Z - constrained zonotope
 %   Outputs:
-%       result - returns the AH-polytope representation of Z.
+%       [G, c, H, k] - returns the AH-polytope representation matrices of Z.
 %
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 
-function [result] = conZono2AHPoly(Z)
+function [G,c,H,k] = conZono2AHPoly(Z)
 
 if Z.nC == 0
-    result.G = Z.G;
-    result.c = Z.c;    
-    result.A = [-eye(Z.nG);eye(Z.nG)];
-    result.b = ones(2*Z.nG,1);
+    G = Z.G;
+    c = Z.c;    
+    H = [-eye(Z.nG);eye(Z.nG)];
+    k = ones(2*Z.nG,1);
 else
     T = null(Z.A,'r');  % Computes the right null space of A.
-    s = pinv(Z.A)*obj.b;
+    s = pinv(Z.A)*Z.b;
     P = Polyhedron('H',[T ones(size(s,1),1)-s; -T ones(size(s,1),1)+s]); % Inequality constraints on \xi variables.
     P = minHRep(P); % Removes redundant halfspaces
-    result.G = Z.G*T;
-    result.c = Z.c+Z.G*s;    
-    result.A = P.H(:,1:end-1);
-    result.b = P.H(:,end);
+    G = Z.G*T;
+    c = Z.c+Z.G*s;    
+    H = P.H(:,1:end-1);
+    k = P.H(:,end);
 end
 
 end
