b29430f0dd097b2a8a533f59442c1ce927671ff2
Jonah Glunt <jjg56@psu.edu>
2024-08-06 11:29:51 -0400

    testing various dimensions

--- dev/testing/Testing_removeRedundancy.m
+++ dev/testing/Testing_removeRedundancy.m
@@ -1,23 +1,29 @@
 clc, close all, clear all
 
 flag_plots = false;
-which_to_plot = [8];
+which_to_plot = [];
 rng(2)
 
 data = nan(0,3);
 
-num_trials = 10;
+num_trials = 20;
 attempts = nan(num_trials, 1);
 Z = repmat({nan}, num_trials, 1);
-Z_rr = Z;
+Z_rr = Z; 
+times = nan(num_trials, 1);
 for i = 1:num_trials
-    Z{i} = randomSet(randi(2^32), 'hybZono', 4, randi(100), randi(10), randi(10));
+    Z{i} = randomSet(randi(2^32), 'hybZono', 10, 50, 10, 30);
     attempts(i) = 1;
     while is_empty(Z{i})
-        Z{i} = randomSet(randi(2^32), 'hybZono', 4, randi(100), randi(10), randi(10));
+        Z{i} = randomSet(randi(2^32), 'hybZono', 10, 50, 10, 30);
         attempts(i) = attempts(i)+1;
     end
+%     Z{i}.b = [Z{i}.b; 2*Z{i}.b];
+%     Z{i}.Ab = [Z{i}.Ab; 2*Z{i}.Ab];
+%     Z{i}.Ac = [Z{i}.Ac; 2*Z{i}.Ac];
+    tic
     Z_rr{i} = removeRedundancy(Z{i});
+    times(i) = toc;
     M = eye(3, Z{i}.n);
     disp(i)
     
@@ -39,7 +45,7 @@ for i = 1:num_trials
     data(i,:) = [i Z{i}.nC Z_rr{i}.nC];
 end
 
-table(data(:,1), data(:,2), data(:,3), 'VariableNames', {'i', 'nC (orig)', 'nC (red)'})
+table(data(:,1), data(:,2), data(:,3), times, 'VariableNames', {'i', 'nC (orig)', 'nC (red)', 'seconds'})
 
 return
 %%
