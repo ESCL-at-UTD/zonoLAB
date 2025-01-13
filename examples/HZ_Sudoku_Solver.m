e973a8b267082eaa0b89ab67e8aefd9d314f9b76
Jonah Glunt <jjg56@psu.edu>
2025-01-13 16:53:39 -0500

    began adding some comments/discussion to this script

--- examples/HZ_Sudoku_Solver.m
+++ examples/HZ_Sudoku_Solver.m
@@ -1,24 +1,6 @@
-% CLUES = [1,2,2;
-%     1,5,3;
-%     1,8,4;
-%     2,1,6;
-%     2,9,3;
-%     3,3,4;
-%     3,7,5;
-%     4,4,8;
-%     4,6,6;
-%     5,1,8;
-%     5,5,1;
-%     5,9,6;
-%     6,4,7;
-%     6,6,5;
-%     7,3,7;
-%     7,7,6;
-%     8,1,4;
-%     8,9,8;
-%     9,2,3;
-%     9,5,4;
-%     9,8,2];
+% First, list all of the Clues that are given on the Sodoku board. If
+% square (i,j) takes the value v, then append the triplet [i j v] to the
+% matrix below. 
 
 CLUES = [1 1 6
          1 2 5
@@ -47,16 +29,17 @@ CLUES = [1 1 6
          9 8 7
          9 9 5];
 
-
 drawSudoku(CLUES)
-
+title('Question')
 
 %%
 
-% for the first attempt, let's try the method of just 9 binary variables
-% per square (this results in 729 binary variables, which is bad)
-
-% each square have binary variables {xi_(i,j,1),...,xi_(i,j,9)}
+% For the most straight-forward first attempt, let's try the method of
+% having 9 binary variables per square (this results in 729 binary 
+% variables). This is inefficient, and could be reduced to as little as 4
+% binary variables per square, but then the constraints become much more
+% difficult to write. And I have found that Gurobi has no troubles handling
+% the HZs that result from this code. 
 
 Ac = [];
 numCons = 4*81;
@@ -64,6 +47,9 @@ numHints = size(CLUES, 1);
 Ab = zeros(numCons, 9^3);
 thisCon = 0;
 
+% the first numCons constraints come from the rules of solving a Sudoku
+% puzzle, and the next numHints constraints are the hints given to us to
+% begin the puzzle. 
 b = [-7*ones(numCons, 1); ones(numHints,1)];
 
 % each square gets a "sum-to-one" constraint (along the "value"-axis)
@@ -118,11 +104,15 @@ for I = 1:3
     end
 end
 
+% and now here we constrain our solution to match the hints we were given
 for m = 1:size(CLUES, 1)
     thisCon = thisCon+1;
     Ab(thisCon, map(CLUES(m,1), CLUES(m,2), CLUES(m,3))) = 1;
 end
 
+% time to make the hybzono; we don't actually care about the generators at
+% all (we really just want to solve a feasibility problem), so here I just
+% set them all zero. 
 Gc = [];
 Gb = zeros(1,9^3);
 c = zeros(1,1);
@@ -134,6 +124,9 @@ Phi = hybZono(Gc, Gb, c, Ac, Ab, b);
 numLeaves = size(getLeaves(Phi,[]),2);
 disp(['There is/are ' num2str(numLeaves) ' possible solution/s.'])
 
+% if multiple leaves are found, then either: i) there is a bug in the code,
+% or ii) the hints to the Sudoku board do not properly constrain you to a
+% single solution. 
 %%
 SOL = [];
 if numLeaves == 1
@@ -148,18 +141,23 @@ if numLeaves == 1
     end
     drawSudoku(SOL)
 end
-
+% you could edit this section to have it solve the sudoku board for all of
+% the leaves (when there are multiple)
 %%
 
+% A triplet (i,j,k) corresponds with one possible value of one possible
+% number on the Sudoku board. For example, (1,2,3) indicates the binary
+% variable which determines whether or not square (1,2) takes the value 3.
+% This function determines which of the 9^3 binary variables corresponds to
+% the triplet (i,j,k)
 function dim = map(i,j,k)
     dim = 81*(i-1) + 9*(j-1)+k;
 end
 
+% I found this function online
 function drawSudoku(CLUES)
 % Function for drawing the Sudoku board
-
 %   Copyright 2014 The MathWorks, Inc. 
-
 
 figure;hold on;axis off;axis equal % prepare to draw
 rectangle('Position',[0 0 9 9],'LineWidth',3,'Clipping','off') % outside border
