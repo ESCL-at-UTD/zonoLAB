5d827dfc61dfc080845645f31899c237659df6de
Glunt <jjg57@psu.edu>
2025-04-15 14:23:37 -0400

    removed sections that had nothing to do with convex hulls

--- dev/testing/Testing_convexHull.m
+++ dev/testing/Testing_convexHull.m
@@ -28,74 +28,6 @@ plot(Z, 'g', 0.3)
 legend({'$\mathcal{X}$','$\mathcal{Y}$','$\mathcal{Z} = \textbf{hull}(\mathcal{X} \cup \mathcal{Y})$'}, 'Location','eastoutside','Interpreter','latex')
 
 %%
-
-clear all, close all, clc
-rng_seed = 42;
-rng(rng_seed);
-
-X.c = zeros(4,1);
-X.Gc = randn(4,4);
-X.Gb = 10*randn(4,6);
-X.Ac = [randn(1,4); zeros(1,4)];
-X.Ab = [zeros(1,6); 1 0 1 0 0 0];
-X.b = zeros(2,1);
-X = hybZono(X.Gc, X.Gb, X.c, X.Ac, X.Ab, X.b);
-
-try
-    figure, plot(X)
-catch
-    warning('Can''t plot a 4D set')
-end
-
-%%
-
-clear, close all, clc
-
-c = zeros(2,1);
-G = [1 0 1
-     0 10 -3];
-A = [1 1 1];
-b = 0;
-X = zono(G, c);
-Y = conZono(G, c, A, b);
-
-figure
-hold on, grid on, grid minor
-plot(X, 'b', .5)
-plot(Y, 'g', .5)
-
-DX = zono(eye(3), zeros(3,1));
-DY = conZono(eye(3), zeros(3,1), A, b);
-
-figure
-hold on, grid on, grid minor
-plot(DX, 'm', 0.2)
-plot(DY, 'r', 0.7)
-
-%%
-clear, close all, clc
-
-c = zeros(2,1);
-G = 5*randn(2,2);
-A = [1 1];
-b = .25;
-X = zono(G, c);
-Y = conZono(G, c, A, b);
-
-figure
-hold on, grid on, grid minor
-plot(X, 'b', .5)
-plot(Y, 'g', .5)
-
-DX = zono(eye(2), zeros(2,1));
-DY = conZono(eye(2), zeros(2,1), A, b);
-
-figure
-hold on, grid on, grid minor
-plot(DX, 'm', 0.2)
-plot(DY, 'r', 0.7)
-
-%%
 clear, close all, clc
 
 X = randomSet(1, 'conZono', 3, 4, 4, 1);
@@ -105,6 +37,7 @@ figure, hold on, grid on, grid minor
 plot(X, 'b', 0.5)
 plot(Y, 'r', 0.5)
 plot(Z, 'g', 0.3)
+
 %%
 clear, close all, clc
 rng_seed = 44;
