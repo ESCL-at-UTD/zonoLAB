22d94380b944b896a8cf3d564828f93d30911ed9
Aayush Dulal <65671083+aayushdD@users.noreply.github.com>
2026-03-10 20:17:42 -0500

    Clean up comments in Readme.m
    Removed outdated comments about the orderReduction method.

--- @abstractZono/Readme.m
+++ @abstractZono/Readme.m
@@ -1,10 +1,3 @@
 
 
-% The obj=orderReduction(obj,setType,orderReductionTechnique,final_order)
-% method takes the object, type of object (zonotope or other type of set),
-% reduction technique (exact or something else) and final order we desire.
-% setType and orderReductionTechnique are optional arguments and have
-% default values of 'Zonotope' and 'Exact'. Currently the reduction
-% techniques are provided only for zonotopes. Hence, exact, inner or outer
-% approximation for zonotopes can be done. There could be some
-% inconsistencies. The object is an object from abstract Zono class.
+
